cc78d010b2f390bd88e4c86bc2e1a3bddbe659f9
sato akihide <aki@sato.local>
2019-10-02 21:16:37 +0900

    Add: トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'top#index'
   match '*path' => 'application#routing_error', via: :all
 
   if Rails.env.development?
